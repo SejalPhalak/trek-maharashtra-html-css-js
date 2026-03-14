eb9b7712423595d2e94f32a789459dc37c077daf
vidyasonawane724 <vidyasonawane724@gmail.com>
2026-03-14 17:03:21 +0530

    changes updated

--- js/login.js
+++ js/login.js
@@ -1,10 +1,10 @@
-function toggleMenu(){
+function toggleMenu() {
 
-let menu = document.getElementById("navMenu");
-let auth = document.querySelector(".auth-links-containar");
+    let menu = document.getElementById("navMenu");
+    let auth = document.querySelector(".auth-links-containar");
 
-menu.classList.toggle("active");
-auth.classList.toggle("active");
+    menu.classList.toggle("active");
+    auth.classList.toggle("active");
 }
 
 
@@ -22,35 +22,35 @@ function togglePassword() {
     }
 }
 
-function loginUser(){
+function loginUser() {
 
-let email = document.getElementById("email").value;
-let password = document.getElementById("password").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
 
-if(email === "" || password === ""){
-alert("Please fill all information");
-return;
+    if (email === "" || password === "") {
+        alert("Please fill all information");
+        return;
+    }
+
+    /* Example username from email */
+    let username = email.split("@")[0];
+
+    /* Show username */
+    document.getElementById("welcomeUser").innerText = "Welcome " + username + " 👋";
+
+    /* hide login + welcome card */
+    document.querySelector(".login-box").style.display = "none";
+    document.querySelector(".welcome-card").style.display = "none";
+
+    /* show profile */
+    document.getElementById("profileSection").style.display = "block";
+
 }
 
-/* Example username from email */
-let username = email.split("@")[0];
+function logoutUser() {
 
-/* Show username */
-document.getElementById("welcomeUser").innerText = "Welcome " + username + " 👋";
-
-/* hide login + welcome card */
-document.querySelector(".login-box").style.display="none";
-document.querySelector(".welcome-card").style.display="none";
-
-/* show profile */
-document.getElementById("profileSection").style.display="block";
-
-}
-
-function logoutUser(){
-
-document.querySelector(".login-box").style.display="block";
-document.querySelector(".welcome-card").style.display="block";
-document.getElementById("profileSection").style.display="none";
+    document.querySelector(".login-box").style.display = "block";
+    document.querySelector(".welcome-card").style.display = "block";
+    document.getElementById("profileSection").style.display = "none";
 
 }
